19157d8b64b17d956224a41eb6f954e0c47298e0
Bruno02468 <bruno@paschoalinoto.com>
2015-03-09 20:23:37 -0300

    Atualizado via script

---
+++ conta.php
@@ -0,0 +1,10 @@
+<?php
+    
+    $contador = "contador.txt";
+    $last = file_get_contents($contador);
+    
+    $resultado = $last + 1;
+    file_put_contents($contador, $resultado);
+    echo $resultado;
+    
+?>
