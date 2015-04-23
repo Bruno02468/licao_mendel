904fd8f6d3b6e5890b73621b09c7b98bc77deea2
= <bruno@paschoalinoto.com>
2015-04-23 19:53:15 -0300

    Suporte a matérias de provas

--- agenda.php
+++ agenda.php
@@ -30,13 +30,28 @@
         if ('.qc' === $file) continue;
         if ($pasta . 'index.php' === $file) continue;
         
-        $final = "<acronym title='ID de lição: " . basename($file) . "'><table>\n";
-        
         $arquivo = file($file);
+        
+        $mat = trim($arquivo[0]);
+        $v = "fez";
+        $ent = "de entrega";
+        $gabaritei = "Gabaritei";
+        $classe = "";
+            
+        if (strpos($mat, "PROVA - ") !== false) {
+            $mat = str_replace("PROVA - ", "", $mat);
+            $v = "estudou";
+            $ent = "da prova";
+            $gabaritei = "Estou careca de estudar";
+            $classe = " class='prova'";
+        }
+        
+        $final = "<acronym title='ID de lição: " . $file . "'><table$classe>\n";
+        
         $datacri = filectime($file);
         $pass = date("d/m/Y", $datacri);
-        $materia = "<tr><td valign='top'><span class='semiimportante'>Matéria:</span> </td><td valign='top'>" . trim($arquivo[0]) . "<br></td></tr>\n";
-        $passada = "<tr><td valign='top'><span class='semiimportante'>Passada em:</span> </td><td valign='top'>" . $pass . "<br></td></tr>";
+        $materia = "<tr><td valign='top'><span class='semiimportante'>Matéria:</span> </td><td valign='top'>$mat<br></td></tr>\n";
+        $passada = "<tr><td valign='top'><span class='semiimportante'>Passada em:</span> </td><td valign='top'>$pass<br></td></tr>";
         $datastr = $arquivo[1];
         $entrega = strtotime($datastr);
         
@@ -49,7 +64,7 @@
         if (date("d/m/Y", $amanha) == $datafin) {
             $datafin .= "<b> (amanhã)</b>";
         }
-        $datapre = "<tr><td valign='top'><span class='semiimportante'>Data de entrega:</span> </td><td valign='top'>" . $datafin . "<br></td></tr>\n";
+        $datapre = "<tr><td valign='top'><span class='semiimportante'>Data $ent:</span> </td><td valign='top'>$datafin<br></td></tr>\n";
         
         $dadosarr = $arquivo;
         unset($dadosarr[0]);
@@ -57,7 +72,7 @@
         $dados = "<tr><td valign='top'><span class='semiimportante'>Informações:</span> </td><td valign='top'>" . join("<br>", $dadosarr) . "<br></td></tr>\n";
         $final .= $materia;
         
-        $check = "<tr><td valign='top'><span class='semiimportante'>Já fez?</span> </td><td valign='top'><input type='checkbox' id='" . basename($file) . "' onclick='toggleFeita(this.id)'> Gabaritei<br></td></tr>\n";
+        $check = "<tr><td valign='top'><span class='semiimportante'>Já $v?</span> </td><td valign='top'><input type='checkbox' id='" . basename($file) . "' onclick='toggleFeita(this.id)'>$gabaritei<br></td></tr>\n";
         if ($pass == $hoje) {
             $final .= $datapre . $dados. $check . "</table></acronym><br><br>\n";
             $hojes .= $final;
--- index.php
+++ index.php
@@ -29,10 +29,26 @@
             if ('.qc' === $file) continue;
             if ('index.php' === $file) continue;
             
-            $final = "<acronym title='ID de lição: " . $file . "'><table>\n";
             
             $arquivo = file($pasta . $file);
-            $materia = "<tr><td valign='top'><span class='semiimportante'>Matéria:</span> </td><td valign='top'>" . trim($arquivo[0]) . "<br></td></tr>\n";
+            
+            $mat = trim($arquivo[0]);
+            $v = "fez";
+            $ent = "de entrega";
+            $gabaritei = "Gabaritei";
+            $classe = "";
+            
+            if (strpos($mat, "PROVA - ") !== false) {
+                $mat = str_replace("PROVA - ", "", $mat);
+                $v = "estudou";
+                $ent = "da prova";
+                $gabaritei = "Estou careca de estudar";
+                $classe = " class='prova'";
+            }
+            
+            $final = "<acronym title='ID de lição: " . $file . "'><table$classe>\n";
+            
+            $materia = "<tr><td valign='top'><span class='semiimportante'>Matéria:</span> </td><td valign='top'>$mat<br></td></tr>\n";
             $datastr = $arquivo[1];
             $entrega = strtotime($datastr);
             
@@ -41,15 +57,17 @@
                 continue;
             }
             
+            
+            
             $datafin = date("d/m/Y", $entrega); 
-            $datapre = "<tr><td valign='top'><span class='semiimportante'>Data de entrega:</span> </td><td valign='top'>" . $datafin . "<br></td></tr>\n";
+            $datapre = "<tr><td valign='top'><span class='semiimportante'>Data $ent:</span> </td><td valign='top'>$datafin<br></td></tr>\n";
             
             $dadosarr = $arquivo;
             unset($dadosarr[0]);
             unset($dadosarr[1]);
             $dados = "<tr><td valign='top'><span class='semiimportante'>Informações:</span> </td><td valign='top'>" . join("<br>", $dadosarr) . "<br></td></tr>\n";
             
-            $check = "<tr><td valign='top'><span class='semiimportante'>Já fez?</span> </td><td valign='top'><input type='checkbox' id='" . $file . "' onclick='toggleFeita(this.id)'>Gabaritei<br></td></tr>\n";
+            $check = "<tr><td valign='top'><span class='semiimportante'>Já $v?</span> </td><td valign='top'><input type='checkbox' id='$file' onclick='toggleFeita(this.id)'>$gabaritei<br></td></tr>\n";
             $final .= $materia;
             
             
