3d058215c34e286bcf3bc3e316c021e0b6cb61c2
= <bruno@paschoalinoto.com>
2015-04-26 19:07:13 -0300

    hue

---
+++ ademir/atuadores/deleta.php
@@ -0,0 +1,16 @@
+<?php
+    $sala = htmlspecialchars(req('sala'));
+    $id = htmlspecialchars(req('id'));
+    
+    function req($str) {
+        if (!isset($_GET[$str])) {
+            die("Variável GET \"" . $str . "\" necessária para esta requisição.");
+        } else {
+            return $_GET[$str];
+        }
+    }
+    
+    $arquivo = "../../salas/" . $sala . "/" . $id;
+    unlink($arquivo);
+    
+?>
