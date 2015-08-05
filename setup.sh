da9f1cdad4cb50cd424c9f386bfd526dbf5383e9
= <bruno@paschoalinoto.com>
2015-08-05 19:36:19 -0300

    contador alienado

--- setup.sh
+++ setup.sh
@@ -4,12 +4,14 @@ echo "Criando pastas..."
 mkdir salas
 mkdir salas/1E
 
-echo "Criando arquivo de MOTD..."
+echo "Criando arquivos padrão..."
 touch ademir/atuadores/motd.txt
 echo "[insira mensagem do dia aqui]" > ademir/atuadores/motd.txt
+touch extras/contador.txt
 
 echo "Setando permissões..."
 chmod -R 777 salas
 chmod 777 ademir/atuadores/motd.txt
+chmod 777 extras/contador.txt
 
-echo "Tudo operante."
+echo "Tudo pronto."
