5e7b7c813bc467104913d55cac46007f289bd1b9
= <bruno@paschoalinoto.com>
2015-10-18 14:41:35 -0200

    localstoage ftw

--- extras/javascript.js
+++ extras/javascript.js
@@ -17,42 +17,28 @@ function vlw() {
     document.getElementById("vlw").innerHTML = ajaxGet("conta.php");
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
+var feitas;
+eval("feitas = [" + (localStorage["feitas"] || "") + "];");
 
-var feito;
-if (document.cookie.indexOf("feito") < -1) {
-    document.cookie = "feito=; path=/licao;";
-} else {
-    feito = getCookie("feito");
-    var n = feito.split(",");
-    for (c in n) {
-        var id = n[c];
-        if (!id)
-            continue;
-        var check = document.getElementById(id);
-        if (!check) {
-            toggleFeita(id);
-            continue;
-        }
-        check.checked = true;
-    }
-}
 
 function toggleFeita(id) {
-    if (getCookie("feito").indexOf("," + id) > -1) {
-        document.cookie = document.cookie.replace("," + id, "");
-    } else {
-        document.cookie = "feito=" + getCookie("feito") + "," + id + "; path=/licao;";
+    id = parseInt(id);
+    var i = feitas.indexOf(id);
+    if (i > -1)
+        feitas.splice(i, 1);
+    else
+        feitas.push(parseInt(id));
+    localStorage["feitas"] = feitas.toString();
+}
+
+for (var index in feitas) {
+    var id = parseInt(feitas[index]);
+    var checkbox = document.getElementById(id);
+    if (!checkbox) {
+        toggleFeita(id);
+        continue;
     }
+    checkbox.checked = true;
 }
 
 function horario() {
