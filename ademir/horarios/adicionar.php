de9c45999e442d0bdd9543655e1d65c80a499f31
= <bruno@paschoalinoto.com>
2015-12-25 01:13:57 -0200

    sai ph

--- ademir/horarios/adicionar.php
+++ ademir/horarios/adicionar.php
@@ -12,7 +12,7 @@ include("../../extras/funcs.php");
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     </head>
     <body style="text-align: center;">
-        <h1>Painel Administrativo</h1>
+        <h1>Adicionar horário</h1>
         <small>Tudo programado por Bruno Borges Paschoalinoto (1ª E)</small>
         <br>
         <br>
--- ademir/horarios/editar.php
+++ ademir/horarios/editar.php
@@ -21,7 +21,7 @@ foreach ($arq as $line) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     </head>
     <body style="text-align: center;">
-        <h1>Painel Administrativo</h1>
+        <h1>Editar horário</h1>
         <small>Tudo programado por Bruno Borges Paschoalinoto (1ª E)</small>
         <br>
         <br>
