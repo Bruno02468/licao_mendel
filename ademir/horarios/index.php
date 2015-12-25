27872c4ac4d1ae385564d9854f77e90841ef1319
= <bruno@paschoalinoto.com>
2015-12-25 01:16:10 -0200

    sai ph²

--- ademir/horarios/index.php
+++ ademir/horarios/index.php
@@ -23,7 +23,7 @@ foreach ($scan as $sala) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     </head>
     <body style="text-align: center;">
-        <h1>Painel Administrativo</h1>
+        <h1>Horários das salas</h1>
         <small>Tudo programado por Bruno Borges Paschoalinoto (1ª E)</small>
         <br>
         <br>
