c4982d93cbd5e0dcb2a780a213ac058a15abba3a
= <bruno@paschoalinoto.com>
2016-02-01 20:46:13 -0200

    horas das aulas

--- extras/horario2html.php
+++ extras/horario2html.php
@@ -15,17 +15,25 @@ function maketd($s) {
 $sala = req_get("sala");
 $arq = file("../ademir/horarios/hors/$sala.horario");
 $others = "";
-$intervalo = "<tr class=\"horgray\"><td><b>Intervalo</b></td><td><b>Intervalo</b></td><td><b>Intervalo</b></td><td><b>Intervalo</b></td><td><b>Intervalo</b></td><td><b>Intervalo</b></td></tr>";
+
+function intervalo($n) {
+    $time = ["9:20", "11:15"][$n-1];
+    return "<tr class=\"horgray\"></td><td><b>${n}º Intervalo ($time)</b></td><td><b>Intervalo</b></td><td><b>Intervalo</b></td><td><b>Intervalo</b></td><td><b>Intervalo</b></td><td><b>Intervalo</b></td></tr>";
+}
+
+
 
 for ($aula = 1; $aula <= 8; $aula++) {
-    $others .= "<tr><td class=\"hororange\">${aula}ª Aula</td>";
+    $aulatimes = ["7:05", "7:50", "8:35", "9:45", "10:30", "11:40", "12:25", "13:10"];
+    $time = $aulatimes[$aula-1];
+    $others .= "<tr><td class=\"hororange\">${aula}ª Aula ($time)</td>";
     for ($dia = 0; $dia <= 4; $dia++) {
         $mat = substituir_global("/;/", "", trim($arq[($aula-1)*5 + $dia]));
         $others .= maketd($mat);
     }
     $others .= "</tr>";
     if ($aula == 3 || $aula == 5)
-        $others .= $intervalo;
+        $others .= intervalo(floor($aula / 2));
     $others .= "\n";
 }
 
