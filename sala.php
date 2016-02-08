b921b206f4faff0acd54faccef9bfe7dd22e534b
= <bruno@paschoalinoto.com>
2016-02-08 01:16:24 -0200

    mini-fix do SUPER UPDATE

--- sala.php
+++ sala.php
@@ -77,8 +77,8 @@ foreach ($licoes as $id => $licao) {
 
     $tabela = "<table class=\"entrada$classes\"$display>\n";
 
-    $tabela .= make_tr("Matéria:", $licao["materia"])
-        . make_tr("Informações:", $licao["info"])
+    $tabela .= make_tr("Matéria:", formatar($licao["materia"]))
+        . make_tr("Informações:", formatar($licao["info"]))
         . make_tr("Para:", ($proxima ? "<b>$perto</b>" : ($parahj ? "<b>Hoje</b>" : (date("d/m/Y", dataToTime($licao["para"])) . " (<b>$semanal</b>)"))))
         . make_tr("Feita?", "<input type=\"checkbox\" id=\"$id\" onclick=\"toggleFeita(this.id)\">Feita!");
 
--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/sh
 
-echo "Criando pastas..."
-mkdir salas
-
 echo "Criando arquivos padrão..."
 touch superademir/atuadores/motd.txt
-touch superademir/auth/.shadow
-touch superademir/atuadores/ademires.txt
+touch extras/.supershadow
+touch extras database.json
 echo "[insira mensagem do dia aqui]" > superademir/atuadores/motd.txt
 
 echo "Setando permissões..."
-chmod -R 777 salas
 chmod 777 superademir/atuadores/motd.txt
-chmod -R 777 ademir/horarios/hors
-chmod 777 superademir/auth/.shadow
-chmod 777 superademir/atuadores/ademires.txt
+chmod 777 extras/.supershadow
+chmod 777 extras database.json
 
 echo "Tudo pronto."
