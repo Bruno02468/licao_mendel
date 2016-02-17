e5c54f169699b46f7e1a1da488c2bbd69946b76f
= <bruno@paschoalinoto.com>
2016-02-17 14:53:52 -0200

    chega de pretty print

--- extras/database.php
+++ extras/database.php
@@ -122,7 +122,7 @@ function getFullJSON() {
 
 function setNewJSON($new_json) {
     file_put_contents(currentDir() . "database.json", json_encode($new_json, JSON_HEX_TAG
-    | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT), LOCK_EX);
+    | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE), LOCK_EX);
 }
 
 function addSala($id, $opaque, $salt, $ademir) {
